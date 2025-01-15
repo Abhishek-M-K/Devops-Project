bce24b0131a684145d56e5bd1d674f332d7d1950
Abhishek-M-K <khandareabhishek98@gmail.com>
2025-01-15 20:37:45 +0530

    refactor: nginx elysiajs port 8888

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "kube-nodes" {
 
                   # Tunnel proxy reqs to ElysiaJs App
                   location /api {
-                    proxy_pass http://127.0.0.1:3000; 
+                    proxy_pass http://127.0.0.1:8888; 
                     proxy_http_version 1.1;
                     proxy_set_header Upgrade $http_upgrade;
                     proxy_set_header Connection 'upgrade';
@@ -40,7 +40,7 @@ resource "aws_instance" "kube-nodes" {
 
                 # Restart system and Nginx service
                 systemctl restart nginx
-                
+
                 EOF
 
   tags = {
